3490e24ef030189397e674a0542f691b8cdbc380
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-28 15:38:05 -0700

    added header link for home

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Views/Home/Home';
-
+import Header from './components/Header/Header';
 function App() {
   return (
     <BrowserRouter>
       <div className="App"></div>
+      <Header />
       <Switch>
         <Route exact path="/">
           <Home />
